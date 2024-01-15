27df0a859c0468c3830c16b21c2d659d1cecafa9
john-antony <j.antony3001@gmail.com>
2024-01-14 21:41:15 -0500

    fully updated navbar and search results so they are responsive!

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -74,8 +74,14 @@ const Navbar = () => {
             <Link to="/home" className='home-link'>
                 <h1 className='fashionscape-home'>Fashionscape</h1>
             </Link>
-            <div className='input-searchbar'>
+            <div className='searchbar-container'>
                 <SearchOutlinedIcon className='search-icon'/>
+                {searchTerm && (
+                    <CancelOutlinedIcon
+                        className='clear-button'
+                        onClick={clearSearch}
+                    />
+                )}
                 <input 
                     type='text' 
                     id='searchbar' 
@@ -86,62 +92,56 @@ const Navbar = () => {
                     onChange={handleInputChange}
                 />
                 {searchTerm && (
-                    <CancelOutlinedIcon
-                        className='clear-button'
-                        onClick={clearSearch}
-                    />
-                )}
-                {searchTerm && (
-                    <div className='search-results'>
-                        <div className='user-results'>
-                            <h3 className='search-result-header'>Users:</h3>
-                            <ul>
-                                {searchResults.users.map((user) => (
-                                    <Link className='search-result-link' to={`/profile/${user.username}`}>
-                                        <li key={user.id}>
-                                            <span className='search-result-bubble'>{user.username}</span>
-                                        </li>
-                                    </Link>
-                                ))}
-                            </ul>
-                        </div>
-                        <div className='post-results'>
-                            <h3 className='search-result-header'>Posts:</h3>
-                            <ul>
-                                {searchResults.posts.map((post) => (
-                                    <Link className='search-result-link' to={`/image/${post._id}`}>
-                                        <li key={post.id}>
-                                            <span className='search-result-bubble'>{post.title}</span>
-                                        </li>
-                                    </Link>
-                                ))}
-                            </ul>
-                        </div>
+                <div className='search-results'>
+                    <div className='user-results'>
+                        <h3 className='search-result-header'>Users:</h3>
+                        <ul>
+                            {searchResults.users.map((user) => (
+                                <Link className='search-result-link' to={`/profile/${user.username}`}>
+                                    <li key={user.id}>
+                                        <span className='search-result-bubble'>{user.username}</span>
+                                    </li>
+                                </Link>
+                            ))}
+                        </ul>
                     </div>
-                )}
-                <div className='menu-icons'>
-                    {isLoggedIn ? (
-                            // If user is logged in, display Sign Out button
-                    <button className='signout-button' onClick={handleLogout}>Log Out</button>
-                    ) : (
-                        // If user is not logged in, display Log In button
-                        <Link to="/login">
-                            <button className='signout-button'>Log In</button>
-                        </Link>
-                    )}
-                    <Link to="/create">
-                        <AddCircleIcon className='add-icon'/>
-                    </Link>
-                    <Link to="/chat">
-                        <ChatBubbleIcon className='chat-icon'/>
-                    </Link>
-                    <Link to="/likes">
-                        <FavoriteIcon className='like-icon'/>
-                    </Link>
-                    <Link to={user && user.username ? `/profile/${user.username}` : '/profile'}>
-                        <PersonIcon className='profile-icon'/>          
-                    </Link>
+                    <div className='post-results'>
+                        <h3 className='search-result-header'>Posts:</h3>
+                        <ul>
+                            {searchResults.posts.map((post) => (
+                                <Link className='search-result-link' to={`/image/${post._id}`}>
+                                    <li key={post.id}>
+                                        <span className='search-result-bubble'>{post.title}</span>
+                                    </li>
+                                </Link>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
+                )}
+            </div>
+            <div className='menu-icons'>
+                <Link to="/create">
+                    <AddCircleIcon className='add-icon'/>
+                </Link>
+                <Link to="/chat">
+                    <ChatBubbleIcon className='chat-icon'/>
+                </Link>
+                <Link to="/likes">
+                    <FavoriteIcon className='like-icon'/>
+                </Link>
+                <Link to={user && user.username ? `/profile/${user.username}` : '/profile/Guest'}>
+                    <PersonIcon className='profile-icon'/>          
+                </Link>
+                {isLoggedIn ? (
+                        // If user is logged in, display Sign Out button
+                <button className='signout-button' onClick={handleLogout}>Log Out</button>
+                ) : (
+                    // If user is not logged in, display Log In button
+                    <Link to="/login">
+                        <button className='signout-button'>Log In</button>
+                    </Link>
+                )}
             </div>
         </div>
     );
